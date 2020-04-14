4f694ea0538a6f8ec22dd0a7514cbdb5b8347787
alex-suschin <alex_suschin@mail.ru>
2020-04-14 15:42:59 +0300

    support-all

--- src/blocks/modules-pages/support-vn/support-vn.js
+++ src/blocks/modules-pages/support-vn/support-vn.js
@@ -1,3 +1,31 @@
 $('.js-btn-show-info-quest').click(function() {
     $(this).parents('.quest-item').toggleClass('active');
 });
+
+$('.support-tabs a').click(function() {
+    $('.support-tabs').find('.active').removeClass('active');
+    $(this).addClass('active');
+    $('.support-elems').find('.support-elem').hide();
+    $('#' + $(this).data('switch')).show();
+});
+
+let fileVal = $('.jq-file__name').text();
+
+console.log(fileVal);
+
+if (fileVal != null) {
+    $('.remove-val').show();
+} else {
+    $('.remove-val').hide();
+}
+
+
+// $('.remove-val').click(function() {
+//     console.log(fileVal);
+//     fileVal.text('');
+// });
+
+
+$('.btn-show-history-request').click(function() {
+    $(this).parents('.history-request-cell').parents('.history-request__item-show').parents('.history-request__item').toggleClass('active');
+});
