eda4b53139fad6fee6e5508d4d38fa609397f1ea
alex-suschin <alex_suschin@mail.ru>
2020-04-23 10:27:18 +0300

    without-swipe

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,10 @@ import "./import/jquery.formstyler.min";
 import "./import/form-js.js";
 import "./import/jquery.fancybox.min";
 import "./import/datepicker.min";
-import "./import/jquery.mobile-1.4.5.min";
-import "./import/jquery.touchSwipe.min";
-import "./import/jquery.slideandswipe.min";
+// import "./import/jquery.mobile-1.4.5.min";
+// import "./import/jquery.touchSwipe.min";
+// import "./import/jquery.slideandswipe.min";
 
-$(function() {
-    $('.menu-lk').slideAndSwipe();
-});
+// $(function() {
+//     $('.menu-lk').slideAndSwipe();
+// });
